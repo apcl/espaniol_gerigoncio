d14c8fd971b8cec3fa4dbf2d88b7ed4290b4d95e
Alejandro Cofré <acofre@fia.local>
2016-05-06 16:05:40 -0300

    Subida de gerigoncio.py

---
+++ gerigoncio.py
@@ -0,0 +1,33 @@
+run = True
+while run == True:
+    palabra = input("Que palabra quieres traducir a gerigoncio?: ")
+    lp = []
+    cont = 0
+    v1 = ""
+    v2 = ""
+    for l in palabra:    
+        lp.append(l)    
+        if l == "a":
+            v1 = "a"
+            v2 = "pa"
+        elif l == "e":
+            v1 = "e"
+            v2 = "pe"
+        elif l == "i":
+            v1 = "i"
+            v2 = "pi"
+        elif l == "o":
+            v1 = "o"
+            v2 = "po"
+        elif l == "u":
+            v1 = "u"
+            v2 = "pu"
+
+        if l == v1:
+            lp.insert(cont+1,v2)
+            cont += 1    
+        cont += 1
+    np = ""
+    for i in lp:
+        np += i
+    print(np)
